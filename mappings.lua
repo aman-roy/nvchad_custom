1c43ed7f4579b515918e5da1812158611dacc45b
Aman Roy <admin@amanroy.me>
2023-01-13 10:31:48 +0100

    New plugin: Code runner

--- mappings.lua
+++ mappings.lua
@@ -3,6 +3,7 @@ local M = {}
 M.custom = {
   n = {
     ["<Enter>"] = { "o<Esc>", "Add new line below in normal mode" },
+    ["<leader>r"] = { ":RunCode<CR>", "Run code based on file type" },
   },
 }
 
--- plugins/init.lua
+++ plugins/init.lua
@@ -1,9 +1,15 @@
 return {
-  -- status = {
-  --   dashboard = true,
-  -- },
-  --
-    -- LspConfig
+  -- Reconfiguration of previously installed plugins
+  ["goolord/alpha-nvim"] = {
+    disable = false,
+  },
+  ['williamboman/mason.nvim'] = {
+    override_options = {
+          ensure_installed = { "pyright", "rust-analyzer", "mypy" }
+      }
+  },
+
+  -- LspConfig
   ["neovim/nvim-lspconfig"] = {
       config = function()
         require "plugins.configs.lspconfig"
@@ -11,18 +17,8 @@ return {
       end,
   },
 
+  -- Custom plugin
   ['ggandor/leap.nvim'] = {},
-
-  ['williamboman/mason.nvim'] = {
-    override_options = {
-          ensure_installed = { "pyright", "rust-analyzer", "mypy" }
-      }
-  },
-
-  ["goolord/alpha-nvim"] = {
-    disable = false,
-  },
-
   ["Pocco81/auto-save.nvim"] = {
     config = function ()
       require("auto-save").setup {
@@ -30,4 +26,18 @@ return {
       }
     end,
   },
+  ["CRAG666/code_runner.nvim"] = {
+    requires = 'nvim-lua/plenary.nvim',
+    config = function ()
+      require('code_runner').setup({
+        -- put here the commands by filetype
+        filetype = {
+          java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
+          python = "python3 -u",
+          typescript = "deno run",
+          rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt"
+        },
+      })
+      end
+    },
 }

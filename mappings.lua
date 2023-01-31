baee28ca6599e0305dee4a84095aba6290706eb9
Aman Roy <admin@amanroy.me>
2023-01-31 13:37:18 +0100

    Some new remaps

--- mappings.lua
+++ mappings.lua
@@ -4,7 +4,24 @@ M.custom = {
   n = {
     ["<Enter>"] = { "o<Esc>", "Add new line below in normal mode" },
     ["<leader>r"] = { ":RunCode<CR>", "Run code based on file type" },
+
+    -- LazyGit Config
+    ["<leader>gg"] = { ":LazyGit<CR>", "Start LazyGit" },
+
+
+    -- Copied From ThePrimeagen init.lua repo
+    ["J"] = { "mzJ`z", "Not known" },
+    ["<C-d>"] = { "<C-d>zz", "Move half page down and center page" },
+    ["<C-u>"] = { "<C-u>zz", "Move half page up and center page" },
+    ["n"] = {"nzzzv", "Keep search matches in the middle of the window"},
+    ["N"] = {"Nzzzv", "Keep search matches in the middle of the window"},
   },
+
+  v = {
+    -- Copied From ThePrimeagen init.lua repo
+    ["J"] = {":m '>+1<CR>gv=gv", "Move visually selected line down"},
+    ["K"] = {":m '<-2<CR>gv=gv", "Move visually selected line up"},
+  }
 }
 
 return M
